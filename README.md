1edb78a7fbe04ae9dd99fb71e9020e2a00760117
Prasad Sriramula <psriramula@icloud.com>
2022-02-15 16:17:50 +0000

    Removing android.support dependencies Updating gradle wrapper to 5.4.1-all.zip Updating gradle build to 3.5.3 Adding AndroidX gradle properties
    Signed-off-by: Prasad Sriramula <psriramula@icloud.com>

--- README.md
+++ README.md
@@ -8,13 +8,13 @@
 In this tutorial we will be diving into more Dart programming concepts and learning how to use open source Flutter packages. By the end of this module, you should be comfortable using Flutter packages and building repeated widgets.
 
 
-## What you will create
+## xylophone - flutter app
 
-We’re going to make a music app that plays Xylophone sounds. For every Beethoven out there, this will let you unleash your musical talent where ever you are. 
+project to build xylophone app
 
 ![Finished App](https://github.com/londonappbrewery/Images/blob/master/xylophone-flutter.png)
 
-## What you will learn
+## flutter and art features
 
 - How to incorporate open source libraries of code into your project using Flutter Packages.
 - How to play sound on both iOS and Android.
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  Expanded buildKey({int note, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(note);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(note: 1, color: Colors.red),
+              buildKey(note: 2, color: Colors.orange),
+              buildKey(note: 3, color: Colors.yellow),
+              buildKey(note: 4, color: Colors.green),
+              buildKey(note: 5, color: Colors.teal),
+              buildKey(note: 6, color: Colors.blue),
+              buildKey(note: 7, color: Colors.purple),
+            ],
+          ),
         ),
       ),
     );
